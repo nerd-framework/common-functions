d0b052ef0a05a25e8e28033a2806dafe9da71e55
Roman Lakhtadyr <roman@homefs.biz>
2016-10-14 11:59:36 +0300

    Create Arrays.php

---
+++ src/Nerd/Common/Arrays.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace Nerd\Common\Arrays;
+
+const ANY_USE_VALUE = 0;
+const ANY_USE_KEY = 1;
+const ANY_USE_BOTH = 2;
+
+function any($array, $callable, $option = ANY_USE_VALUE)
+{
+  foreach($array as $key => $value)
+  {
+    switch ($o
+  }
+}
