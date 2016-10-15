d4222b190a50c99ad6ef567a09298fdfd0b62306
Roman Lakhtadyr <roman.lakhtadyr@gmail.com>
2016-10-15 13:51:12 +0300

    test USE_BOTH option

--- tests/ArraysTest.php
+++ tests/ArraysTest.php
@@ -8,6 +8,7 @@ use function Nerd\Common\Arrays\all;
 use function Nerd\Common\Arrays\any;
 
 use const Nerd\Common\Arrays\USE_KEY;
+use const Nerd\Common\Arrays\USE_BOTH;
 
 class ArraysTest extends TestCase
 {
@@ -31,6 +32,16 @@ class ArraysTest extends TestCase
         $this->assertTrue(all([], $test, USE_KEY));
     }
 
+    public function testAllBoth()
+    {
+        $test = function ($value, $key) {
+            return strlen($key) == 2 && $value > 0;
+        };
+        $this->assertFalse(all(["a"  => -10, "b"  => 15, "ab" => 20], $test, USE_BOTH));
+        $this->assertTrue(all(["ab" => 15, "cc" => 20], $test, USE_BOTH));
+        $this->assertTrue(all([], $test, USE_BOTH));
+    }
+
     public function testAnyValue()
     {
         $test = function ($item) {
@@ -50,5 +61,16 @@ class ArraysTest extends TestCase
         $this->assertTrue(any(["aa" => 15, "abc" => 10], $test, USE_KEY));
         $this->assertFalse(any(["aaa" => 155, "abc" => 10], $test, USE_KEY));
         $this->assertFalse(any([], $test, USE_KEY));
+    }
+
+    public function testAnyBoth()
+    {
+        $test = function ($value, $key) {
+            return strlen($key) == 2 && $value > 0;
+        };
+        $this->assertTrue(any(["a"  => -10, "b"  => 15, "ab" => 20], $test, USE_BOTH));
+        $this->assertTrue(any(["ab" => 15, "cc" => 20], $test, USE_BOTH));
+        $this->assertFalse(any([], $test, USE_BOTH));
+        $this->assertFalse(any(["a" => 15, "ab" => -15], $test, USE_BOTH));
     }
 }
